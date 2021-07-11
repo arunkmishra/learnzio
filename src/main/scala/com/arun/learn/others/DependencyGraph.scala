e449ecd72b9453b08a4b8cf329da07e573190f77
Arun <arun@github.com>
2021-07-11 15:51:09 +0530

    version 2 use live instead of def make and zio required dummy defs

--- src/main/scala/com/arun/learn/others/DependencyGraph.scala
+++ src/main/scala/com/arun/learn/others/DependencyGraph.scala
@@ -4,6 +4,9 @@ trait BusinessLogic:
   def doesGoogleHaveEvenAmountOfPictures(topic: String): Boolean
 
 object BusinessLogic:
+  lazy val live: Google => BusinessLogic = //my using this we would not need to pass google to create instance, when using then we can pass
+    fromFunction(make)
+
   def make(google: Google): BusinessLogic =
     new:
       override def doesGoogleHaveEvenAmountOfPictures(topic: String): Boolean =
@@ -12,12 +15,27 @@ trait Google:
   def countPicturesOf(topic: String): Int
 
 object GoogleImpl:
+  lazy val live
+      : Any => Google = // Any is used here just to make live a fxn, and can be ignored when used
+    succeed(make)
   def make: Google =
     new:
       override def countPicturesOf(topic: String): Int =
         if (topic == "cat") 1337 else 1338
 
 object DependencyGraph:
+  lazy val live: Any => BusinessLogic =
+    val google = GoogleImpl.live.apply(())
+    val businessLogicMaker = BusinessLogic.live.provide(google)
+
+    businessLogicMaker
+
+  lazy val liveOld: Any => BusinessLogic =
+    BusinessLogic.live.compose(GoogleImpl.live) // OR GoogleImpl.live.andThen(BusinessLogic.live)
+
+  lazy val makeOld: BusinessLogic =
+    BusinessLogic.make(GoogleImpl.make)
+
   lazy val make: BusinessLogic =
     val google = GoogleImpl.make
     val businessLogic = BusinessLogic.make(google)
@@ -26,9 +44,21 @@ object DependencyGraph:
 
 object RunMain extends scala.App:
 
-  val businessLogic = DependencyGraph.make
+  val businessLogic = DependencyGraph
+    .live
+    .apply(()) //benifit of using live as fxn:  we don't need to create instance at one place
 
   println("-" * 100)
   println(businessLogic.doesGoogleHaveEvenAmountOfPictures("cat"))
   println(businessLogic.doesGoogleHaveEvenAmountOfPictures("dog"))
   println("-" * 100)
+
+extension [R, A](run: R => A)
+  def provide(r: => R): Any => A =
+    _ => run(r)
+
+def succeed[A](a: => A): Any => A =
+  _ => a
+
+def fromFunction[R, A](run: R => A): R => A =
+  r => run(r)
